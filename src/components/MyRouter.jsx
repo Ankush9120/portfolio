4621a73cf0719cda897ba02d468edd5103685f86
Ankush9120 <ankushgupta9120@gmail.com>
2023-05-04 03:02:02 +0530

    updated-skills-page

--- src/components/MyRouter.jsx
+++ src/components/MyRouter.jsx
@@ -5,6 +5,7 @@ import About from "../pages/About";
 import Navbar from "./Navbar";
 import Projects from "../pages/Projects";
 import Contact from "../pages/Contact";
+import Skills from "../pages/Skills";
 
 const MyRouter = () => {
   return (
@@ -15,6 +16,7 @@ const MyRouter = () => {
         <Route path="About" element={<About />} />
         <Route path="Projects" element={<Projects />} />
         <Route path="Contact" element={<Contact />} />
+        <Route path="Skills" element={<Skills />} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,15 +1,85 @@
 import React from "react";
+import Html from "../assets/skillslogo/Html";
+import Css from "../assets/skillslogo/Css";
+import JavaScript from "../assets/skillslogo/JavaScript";
+import ReactIcon from "../assets/skillslogo/ReactIcon";
+import ReduxIcon from "../assets/skillslogo/ReduxIcon";
+import TailwindLogo from "../assets/skillslogo/TailwindLogo";
+import Bootstrap from "../assets/skillslogo/Bootstrap";
 
 const Skills = () => {
   return (
-    <div>
-      <div className="skill">
-        <svg>
-          <circle cx={70} cy={70} r={70}></circle>
-          <circle cx={70} cy={70} r={70}></circle>
-        </svg>
+    <section className="skills">
+      <header className="pt-4">
+        My <span>Skills</span>
+      </header>
+
+      <div className="skillsCover">
+        <div className="skill">
+          <div className="dot"></div>
+          <svg className="circle">
+            <circle cx={70} cy={70} r={70}></circle>
+            <circle cx={70} cy={70} r={70}></circle>
+          </svg>
+          <Html />
+          <span>HTML</span>
+        </div>
+        <div className="skill">
+          <div className="dot"></div>
+          <svg className="circle">
+            <circle cx={70} cy={70} r={70}></circle>
+            <circle cx={70} cy={70} r={70}></circle>
+          </svg>
+          <Css />
+          <span>CSS</span>
+        </div>
+        <div className="skill">
+          <div className="dot"></div>
+          <svg className="circle">
+            <circle cx={70} cy={70} r={70}></circle>
+            <circle cx={70} cy={70} r={70}></circle>
+          </svg>
+          <JavaScript />
+          <span>JS</span>
+        </div>
+        <div className="skill">
+          <div className="dot"></div>
+          <svg className="circle">
+            <circle cx={70} cy={70} r={70}></circle>
+            <circle cx={70} cy={70} r={70}></circle>
+          </svg>
+          <ReactIcon />
+          <span>React</span>
+        </div>
+        <div className="skill">
+          <div className="dot"></div>
+          <svg className="circle">
+            <circle cx={70} cy={70} r={70}></circle>
+            <circle cx={70} cy={70} r={70}></circle>
+          </svg>
+          <ReduxIcon />
+          <span>Redux</span>
+        </div>
+        <div className="skill">
+          <div className="dot"></div>
+          <svg className="circle">
+            <circle cx={70} cy={70} r={70}></circle>
+            <circle cx={70} cy={70} r={70}></circle>
+          </svg>
+          <TailwindLogo />
+          <span>Tailwind</span>
+        </div>
+        <div className="skill">
+          <div className="dot"></div>
+          <svg className="circle">
+            <circle cx={70} cy={70} r={70}></circle>
+            <circle cx={70} cy={70} r={70}></circle>
+          </svg>
+          <Bootstrap />
+          <span>Bootstrap</span>
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
 
